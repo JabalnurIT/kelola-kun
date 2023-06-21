22949e27f8e9af3ac1b4f4a4c2341934772340e2
ghazz1bdw <pavlg5251@gmail.com>
2023-06-21 10:25:37 +0700

    tambah screens

---
+++ FE-Kelola-Kun/lib/screens/not_found_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class NotFoundPage extends StatelessWidget {
+  const NotFoundPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage('assets/PageNotFound.png'),
+          fit: BoxFit.cover,
+        ),
+      ),
+    );
+  }
+}
